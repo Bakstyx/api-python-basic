bcc57888c191213c4bd404adec468313fc5de2ff
Bakstyx <bakstyx.012@gmail.com>
2025-08-01 07:26:50 -0300

    API mock up - poetry definiitons

---
+++ app/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+import redis
+import psycopg2
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/pg")
+def pg_status():
+    conn = psycopg2.connect(host="db", dbname="mydb", user="user", password="password")
+    cur = conn.cursor()
+    cur.execute("SELECT 1")
+    result = cur.fetchone()
+    conn.close()
+    return {"PostgreSQL": result[0]}
+
+
+@app.get("/redis")
+def redis_status():
+    r = redis.Redis(host="redis", port=6379)
+    r.set("key", "value")
+    val = r.get("key")
+    return {"Redis": val.decode()}
